7c7ee85dd37334cc2577b84e9237af85a3c82cbb
celebi law <celebilaw@gmail.com>
2022-08-15 18:09:02 -0700

    navbar implemented

---
+++ thomas-wong-website/src/components/Navbar.js
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import "../css/Navbar.css";
+
+window.onload = function() {
+    const toggleNav = document.getElementsByClassName("toggle-nav")[0];
+    const navBarLinks = document.getElementsByClassName("navbar-links")[0];
+    toggleNav.addEventListener("click", () => {
+        navBarLinks.classList.toggle("active");
+    })
+}
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link className="brand-title" to="/"> 
+                <span> Thomas Wong </span> 
+                <span className="subTitle"> Monterey Park City Council </span> 
+            </Link>
+            <a href="/#" className="toggle-nav">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </a>
+            <div className="navbar-links">
+                <ul>
+                    <li>
+                        <Link className="nav-link" to="/volunteer"> Volunteer </Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link" to="/donate"> Donate </Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link" to="/photos"> Photos </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
