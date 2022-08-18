674e28864709f1db715c237bd558cc882e3dc273
celebi law <celebilaw@gmail.com>
2022-08-17 19:33:48 -0700

    gallery filled with pictures

--- thomas-wong-website/src/components/Gallery.js
+++ thomas-wong-website/src/components/Gallery.js
@@ -18,7 +18,7 @@ function ImageGallery() {
 
 const images = [
   {
-    webUrl: "https://drive.google.com/uc?export=view&id=142IC2nqJ_gdutVYD4IVYtdGlDW-yMY8w",
+    webUrl: "https://drive.google.com/uc?export=view&id=18l-4C6hyAGj--3069Nz9ZGba7rlvnVC5",
     title: 'img1',
   },
   {
@@ -26,27 +26,27 @@ const images = [
     title: 'img2',
   },
   {
-    webUrl: 'https://drive.google.com/uc?export=view&id=18l-4C6hyAGj--3069Nz9ZGba7rlvnVC5',
+    webUrl: 'https://drive.google.com/uc?export=view&id=1p9gxPwswjrkD5d3VWDvuO8UFVrfRPoX_',
     title: 'img3',
   },
   {
-    webUrl: 'https://drive.google.com/uc?export=view&id=1cFI6A0roT0lI2ev7WX-UPRHBhLz6q-R3',
+    webUrl: 'https://drive.google.com/uc?export=view&id=1u3o0h3r-yQOEq_9l2bkzsupJOOCG0v_u',
     title: 'img4',
   },
   {
-    webUrl: 'https://drive.google.com/uc?export=view&id=1u3o0h3r-yQOEq_9l2bkzsupJOOCG0v_u',
+    webUrl: 'https://drive.google.com/uc?export=view&id=142IC2nqJ_gdutVYD4IVYtdGlDW-yMY8w',
     title: 'img5',
   },
   {
-    webUrl: 'https://drive.google.com/uc?export=view&id=1KNLUV_nfExSfV5RF0AGTybyY0TA3Ya3L',
+    webUrl: 'https://drive.google.com/uc?export=view&id=1tfoQihym_0eJU9q2N_9xBDOPGNJ5Gzap',
     title: 'img6',
   },
   {
-    webUrl: 'https://drive.google.com/uc?export=view&id=1wcBC9zG73AR8q2TPYvNd9B8w2pxKUNss',
+    webUrl: 'https://drive.google.com/uc?export=view&id=1_hwxgKYecuD_xSHbiuitrI9dLYrDxcLi',
     title: 'img7',
   },
   {
-    webUrl: 'https://drive.google.com/uc?export=view&id=1j_hyQo61MbZTF_IIH2hovD2FUcKY9Blx',
+    webUrl: 'https://drive.google.com/uc?export=view&id=1LBj9g5LnQYf1tUVxdqSDsaRqexLIQ5H7',
     title: 'img8',
   },
   {
@@ -62,7 +62,7 @@ const images = [
     title: 'img11',
   },
   {
-    webUrl: 'https://drive.google.com/uc?export=view&id=1_hwxgKYecuD_xSHbiuitrI9dLYrDxcLi',
+    webUrl: 'https://drive.google.com/uc?export=view&id=1wcBC9zG73AR8q2TPYvNd9B8w2pxKUNss',
     title: 'img12',
   },
   {
@@ -70,11 +70,11 @@ const images = [
     title: 'img13',
   },
   {
-    webUrl: 'https://drive.google.com/uc?export=view&id=1LBj9g5LnQYf1tUVxdqSDsaRqexLIQ5H7',
+    webUrl: 'https://drive.google.com/uc?export=view&id=1j_hyQo61MbZTF_IIH2hovD2FUcKY9Blx',
     title: 'img14',
   },
   {
-    webUrl: 'https://drive.google.com/uc?export=view&id=1tfoQihym_0eJU9q2N_9xBDOPGNJ5Gzap',
+    webUrl: 'https://drive.google.com/uc?export=view&id=1cFI6A0roT0lI2ev7WX-UPRHBhLz6q-R3',
     title: 'img15',
   },
   {
@@ -88,6 +88,26 @@ const images = [
   {
     webUrl: 'https://drive.google.com/uc?export=view&id=17ewzlqLo9x9BLlbN236ktR1DkX23cVI_',
     title: 'img18',
+  },
+  {
+    webUrl: 'https://drive.google.com/uc?export=view&id=1KNLUV_nfExSfV5RF0AGTybyY0TA3Ya3L',
+    title: 'img19',
+  },
+  {
+    webUrl: 'https://drive.google.com/uc?export=view&id=1x2FpjtNPGRVm_koRjqYV6Ytmgk2pub9r',
+    title: 'img20',
+  },
+  {
+    webUrl: 'https://drive.google.com/uc?export=view&id=1s91PsOyQxVHUFVhjlj2CXJdoSPGjR9W5',
+    title: 'img21',
+  },
+  {
+    webUrl: 'https://drive.google.com/uc?export=view&id=1CGEzXOKjRD7iH_Whpqu2DKLFhm-MZBVP',
+    title: 'img22',
+  },
+  {
+    webUrl: 'https://drive.google.com/uc?export=view&id=1gx1BDEvo07EtULtspBkp-HBvmvBzV7CU',
+    title: 'img23',
   },
 ];
 
